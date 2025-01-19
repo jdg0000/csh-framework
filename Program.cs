d492374bbc44fd0ace624721b86fbed4d096e453
MRBnn400 <yaaboulabany@gmail.com>
2025-01-19 17:33:37 +0300

    alpha 1
    fixed bugs added exceptions

--- Program.cs
+++ Program.cs
@@ -1,27 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-internal class Program
+﻿namespace csh_framework;
+internal static class Program
 {
     public static void Main(string[] args)
     {
+        List<string?> history = new List<string?> { };
+        
         while (true)
         {
             Console.Write("csh1> ");
-            string? input = Console.ReadLine();
-
+            string? input = Console.ReadLine(); 
             int usedOrNot = 0;
+            
 
             int reusi = ((input?.StartsWith("using")) == true) ? 0 : -1;
             int reech = ((input?.StartsWith("echo")) == true) ? 0 : -1;
             int reexi = ((input?.StartsWith("exit")) == true) ? 0 : -1;
+            int requi = ((input?.StartsWith("quit")) == true) ? 0 : -1;
+            int rehis = ((input?.StartsWith("history")) == true) ? 0 : -1;
             int reels = ((input?.StartsWith("ls")) == true) ? 0 : -1;
             int reecd = ((input?.StartsWith("cd")) == true) ? 0 : -1;
+            int recle = ((input?.StartsWith("clear")) == true) ? 0 : -1;
 
             switch (reech)
             {
                 case 0:
                     usedOrNot = 1;
-                    Console.WriteLine(input?.Substring(5, input.Length - 5));   
+                    Console.WriteLine(input?.Substring(5, input.Length - 5));
+                    history.Add(input);
                     break;
             }
 
@@ -29,7 +34,8 @@ internal class Program
             {
                 case 0:
                     usedOrNot = 1;
-                    System.Environment.Exit(0);
+                    history.Add(input);
+                    Environment.Exit(0);
                     break;
             }
 
@@ -37,7 +43,8 @@ internal class Program
             {
                 case 0:
                     usedOrNot = 1;
-                    
+
+                    history.Add(input);
                     DirectoryInfo direct = new DirectoryInfo(".");
                     FileInfo[] files = direct.GetFiles();
 
@@ -51,7 +58,7 @@ internal class Program
                     {
                         Console.WriteLine(dir);
                     }
-                    
+
                     break;
             }
 
@@ -60,7 +67,46 @@ internal class Program
                 case 0:
                     usedOrNot = 1;
 
-                    if (input != null) Directory.SetCurrentDirectory(input.Substring(3, input.Length - 3));
+                    try
+                    {
+                        if (input != null) Directory.SetCurrentDirectory(input.Substring(3, input.Length - 3));
+                    }
+                    catch (DirectoryNotFoundException e)
+                    {
+                        Console.WriteLine("Directory not found");
+                    }
+                    
+                    break;
+            }
+
+            switch (requi)
+            {
+                case 0:
+                    usedOrNot = 1;
+
+                    history.Add(input);
+                    Environment.Exit(0);
+                    break;
+            }
+
+            switch (recle)
+            {
+                case 0:
+                    usedOrNot = 1;
+                    history.Add(input);
+                    Console.Clear();
+                    break;
+            }
+
+            switch (rehis)
+            {
+                case 0:
+                    usedOrNot = 1;
+                    foreach (var t in history)
+                    {
+                        Console.WriteLine(t);
+                    }
+
                     break;
             }
 
